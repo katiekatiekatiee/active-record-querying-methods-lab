8d608317ab76942a551d5263c1c6c1a27db2d921
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-05-06 21:22:14 -0400

    commit tree 58bfe32cf742c4fb8e9bf19df166a0b1bd7ed1e7

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,10 +6,28 @@ class Show < ActiveRecord::Base
     end
 
     def self.most_popular_show
-       Show.where("rating.max").name
+       Show.where("rating = #{highest_rating}").first
     end
 
+    def self.lowest_rating
+        Show.minimum(:rating)
+    end
 
+    def self.least_popular_show
+        Show.where("rating = #{lowest_rating}").first
+    end
 
+    def self.ratings_sum
+        Show.sum(:rating)
+    end
+
+    def self.popular_shows
+        Show.all.where("rating > ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        Show.order(name: :asc)
+    end
+            
 
 end
